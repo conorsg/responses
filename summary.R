a28ed5afaab83ef84a18ea365374094c131ac5fa
Conor <conorsg@gmail.com>
2014-09-04 12:51:10 -0500

    fn to compute crosstable ks tests and return in df

--- summary.R
+++ summary.R
@@ -19,6 +19,9 @@ master$TimeClosed <- as.POSIXct(master$TimeClosed, format="%d-%m-%Y %H:%M:%S")
 master$TimeToDispatch <- difftime(master$TimeDispatch, master$TimeCreate, units="mins")
 master$TimeToArrive <- difftime(master$TimeArrival, master$TimeDispatch, units="mins")
 
+#list o' NOPD districts in dataset
+districts <- c(levels(as.factor(master$District)))
+
 #crime categories
 PropTypes <- c("67P", "55", "67F", "67A", "67", "56", "62", "67S", "62C", "62R", "67B", "56D", "62B", "65", "67E", "67C", "65P", "65J")
 ViolTypes <- c("35D", "35", "34", "34S", "37", "34D", "34C", "37D", "60", "38D")
@@ -70,10 +73,7 @@ master$District <-as.character(master$District)
 arrival_df <- master[master$TimeToArrive >0 & !is.na(master$TimeToArrive), ]
 dispatch_df <- master[master$TimeToDispatch >0 & !is.na(master$TimeToDispatch), ]
 
-#list o' NOPD districts in dataset
-districts <- c(levels(as.factor(master$District)))
-
-#time to arrival density plotting
+#time to arrival density plots
 arrv_signal_dist <- ggplot(arrival_df, aes(x = as.numeric(TimeToArrive), fill = PGroup)) +
                      geom_density(alpha = .3) +
                      geom_vline(xintercept = 5.73, linetype = "dashed", size = .6) +
@@ -101,7 +101,7 @@ arrv_district_dist <- ggplot(arrival_df, aes(x=as.numeric(TimeToArrive), colour 
                        geom_vline(xintercept = 0, colour = "#999999", size = .6) +
                        ggsave("arrv-district-dist.png", width = 12, height = 8)
 
-#time to dispatch density plotting
+#time to dispatch density plots
 disp_signal_dist <- ggplot(dispatch_df, aes(x = as.numeric(TimeToDispatch), colour = PGroup)) +
                     geom_density(alpha = .3) +
                     geom_vline(xintercept = 1.85, linetype = "dashed", size = .6) +
@@ -140,6 +140,13 @@ benchmark_comp <- ggplot(top_priority_calls, aes(x = as.numeric(TimeToDispatch),
                   geom_hline(yintercept = 0, colour = "#999999", size = .6) +
                   geom_vline(xintercept = 0, colour = "#999999", size = .6) +
                   ggsave("signal-dispatch-comp.png", width = 12, height = 8)
+
+#benchmark analysis
+fn_disp <- ecdf(dispatch_df[dispatch_df$PGroup == "two", ]$TimeToDispatch)
+fn_arriv <- ecdf(arrival_df[arrival_df$PGroup == "two", ]$TimeToArrive)
+
+fn_disp(1.85)
+fn_arriv(5.73)
 
 #summary tables
 priority_summary_stats <- ddply(master, "as.character(PGroup)", summarise,
@@ -195,13 +202,6 @@ write.csv(crime_summary_stats, "by-crime.csv", row.names = F)
 write.csv(district_summary_stats, "by-district.csv", row.names = F)
 write.csv(district_high_priority_summary_stats, "by-district-high-priority.csv", row.names = F)
 
-# benchmark analysis
-fn_disp <- ecdf(dispatch_df[dispatch_df$PGroup == "two", ]$TimeToDispatch)
-fn_arriv <- ecdf(arrival_df[arrival_df$PGroup == "two", ]$TimeToArrive)
-
-fn_disp(1.85)
-fn_arriv(5.73)
-
 #shim for fligner test
 shim_disp <- data.frame(dispatch_df$District, dispatch_df$TimeToDispatch)
 shim_arriv <- data.frame(arrival_df$District, arrival_df$TimeToArrive)
@@ -218,9 +218,6 @@ if(fligner_results_arriv$p.value < .05) {
 } else cat("Arrival times between districts are homoskedastic")
 
 #k-s test to find samples with similar shape
-#endgame: ks.test(as.numeric(disp_times_district[[1]]), as.numeric(disp_times_district[[2]]))$p.value
-  #maybe just do two nested for loops? for every n in districts, run through all n in districts?
-
 get_each_district <- function(x, df, column) {
     subset(df, District == x, select = column)
 }
@@ -228,20 +225,25 @@ get_each_district <- function(x, df, column) {
 disp_times_district <- sapply(districts, get_each_district, df = dispatch_df, column = "TimeToDispatch") #creates a recursive list. sub-lists are named. access like so: disp_times_district["1.TimeToDispatch"]
 arriv_times_district <- sapply(districts, get_each_district, df = arrival_df, column = "TimeToArrive")
 
-# ks_tests_dispatch <- ldply(
-#                         .data = disp_times_district,
-#                         .fun = function(x) {
-#                           for(n in districts[districts != "N"]) {
-#                             ks.test(as.numeric(unlist(x)), as.numeric(disp_times_district[[as.numeric(n)]]))$p.value
-#                           }
-#                         })
+#verbose way of creating crosstable ks tests. probably a shortcut in plyr
+crosstable_ks <- function(input, result) {
+  result <- numeric()
 
-# this works. need to store in dataframe
-for (n in districts[districts != "N"]) {
-  for (i in districts[districts != "N"]) {
-    test_col <- rbind(ks.test(as.numeric(disp_times_district[[as.numeric(n)]]), as.numeric(disp_times_district[[as.numeric(i)]]))$p.value)
+  for (n in districts[districts != "N"]) {
+    col <- numeric()
+    for (i in districts[districts != "N"]) {
+      col <- append(col, ks.test(as.numeric(input[[as.numeric(n)]]), as.numeric(input[[as.numeric(i)]]))$p.value)
+      print(col)
+    }
+    result <- cbind(result, col)
   }
+
+  colnames(result) <- districts[districts != "N"]
+  result
 }
+
+ks_dispatch <- crosstable_ks(disp_times_district, ks_dispatch)
+ks_arrival <- crosstable_ks(arriv_times_district, ks_arrival)
 
 #panel charts of distributions
 fbase <- ggplot(dispatch_df, aes(x=as.numeric(TimeToDispatch))) +
